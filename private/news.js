82e339c9b4d622e6d73c9de1d1dc7de0bac17644
SuneQuist44 <cotz4488@gmail.com>
2020-11-05 19:49:55 +0100

    news is up and running!

--- private/news.js
+++ private/news.js
@@ -2,6 +2,8 @@
 // sortContent: Is used for sorting out the HTML tags/elements from the strings content in data. Sends sorted string and result data to our createNewsCard.
 // createNewsCard: Creates DOM from data.
 
+const newsContainer = document.getElementById('news-container');
+
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Fetch content from URI ━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
 const newsContent = (async function () {
     const url = 'https://api.mediehuset.net/infoboard/news';
@@ -16,12 +18,34 @@ function sortContent(data) {
     for (let result of data) {
         let str = result.content
         let regEx = new RegExp(/<.+?>/, 'gi');
-        let shortenString = str.replace(regEx, '').substring(0, 50);
-        createNewsCard(result.title, result.datetime, shortenString)
+        let shortenString = str.replace(regEx, '').substring(0, 250);
+        if (shortenString.length >= 250) shortenString = shortenString + '...';
+
+        let qrCode = createQrCode(shortenString)
+        let date = result.datetime.slice(0, 10)
+        checkForLength(result.title, date, shortenString, qrCode)
     }
 }
 
+function createQrCode(value) {
+    let qrCode = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=' + value;
+    return qrCode;
+}
+
 //━━━━━━━━━━━━━━━━━━━━━━━━━━━━ createNewsCard is our VIEW, which uses data from sortContent ━━━━━━━━━━━━━━━━━━━━━━━━━━━━//
-function createNewsCard(title, date, string) {
-    console.log(title, date, string)
+function checkForLength(title, date, string, qrCode) {
+    let newsCard = document.createElement('div');
+    newsCard.setAttribute('class', 'newsCard')
+    newsContainer.appendChild(newsCard)
+
+    newsCard.innerHTML += `
+        <p class="news-date">${date}</p>
+        <h3 class="news-header">${title}</h3>
+        <p class="news-content">${string}</p>`
+
+    if (string.length > 250) {
+        newsCard.insertAdjacentHTML('beforeend', `        
+        <p class="qr-text">LÆS MERE SKAN QR KODEN</p>
+        <img class="qr-kode" src="${qrCode}" alt="">`)
+    }
 }
